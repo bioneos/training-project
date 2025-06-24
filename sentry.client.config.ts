45fce90fbb6290c724518397b77cbc06ab11386c
Tan Nguyen <tnguyen@bioneos.com>
2025-06-24 09:36:11 -0500

    Revise status code to send report

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,15 +4,16 @@ Sentry.init({
   // If set up, you can use your runtime config here
   dsn: useRuntimeConfig().public.sentry.dsn,
   environment: "development",
-  beforeSend(event) {
+  // This will run before sending any report to Sentry.io
+  beforeSend(event: Sentry.Event) {
     const acceptedStatusCodeList = [
       204, 205, 206, 208, 226,
-      300, 301, 305, 306, 307, 308,
-      405, 406, 407, 408, 409, 410, 412, 413, 415, 416, 417,
+      305, 307, 308,
+      400, 405, 406, 407, 408, 409, 410, 412, 413, 415, 416, 417,
       421, 422, 423, 424, 425, 428, 429, 431, 451,
       500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511
     ];
-    const statusCode = event.breadcrumbs.slice(-1)[0].data.status_code
+    const statusCode = event.breadcrumbs.slice(-1)[0].data.status_code;
 
     if (statusCode) {
       if (acceptedStatusCodeList.indexOf(statusCode) !== -1) {
